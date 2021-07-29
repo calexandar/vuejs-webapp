c6965609c784ebcfda139312f7aa86c79ce96830
calexandar <alexandercvet243@gmail.com>
2021-07-29 14:42:18 +0200

    blogs page aded

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,36 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    sampleBlogCards: [
+      {
+        blogTitle: "Card title goes here",
+        blogCoverPhoto: "stock-1",
+        blogDate: "20 May 2021",
+      },
+      {
+        blogTitle: "Card title goes here",
+        blogCoverPhoto: "stock-2",
+        blogDate: "20 May 2021",
+      },
+      {
+        blogTitle: "Card title goes here",
+        blogCoverPhoto: "stock-3",
+        blogDate: "20 May 2021",
+      },
+      {
+        blogTitle: "Card title goes here",
+        blogCoverPhoto: "stock-4",
+        blogDate: "20 May 2021",
+      },
+       
+    ],
+    editPost: null,
   },
   mutations: {
+    toggleEditPost(state, payload) {
+      state.editPost = payload;
+      
+    }
   },
   actions: {
   },
